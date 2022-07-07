68ee0216510d202e512b9865f28c066f6d7811fc
JWilberLE <wilolopezwilolopez03@gmail.com>
2022-07-06 22:27:57 -0500

    Unidad III. Tarea 1

---
+++ php001.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+        <body>
+            <title>PHP 001</title>
+
+        <h4>Mi Primera Pagina Web Con PHP</h4>
+        <?php
+            echo 'Jordan Wilber Lopez Estrada <br>';
+            echo '3er Cuatrimestre Grupo "A" '
+        ?>
+
+        </body>
+    </html>
